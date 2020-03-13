3befdb16bff2476e9e498b313c5651a8e3cfb00f
Luc Freyermuth <lucfreyermuth@gmail.com>
2020-03-13 16:05:02 +0100

    setting up service to  create menu bar pogramatically

--- projects/desktop-menu-bar/src/lib/desktop-menu-bar.service.ts
+++ projects/desktop-menu-bar/src/lib/desktop-menu-bar.service.ts
@@ -1,8 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ApplicationRef, ComponentFactoryResolver, Injector, ComponentRef } from '@angular/core';
+import { DesktopMenuBarComponent } from '../lib/desktop-menu-bar/desktop-menu-bar.component';
+import { DesktopMenuBarItem } from '../interfaces/desktop-menu-bar-item.interface';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DesktopMenuBarService {
-    constructor() {}
+    menuComponentInstance: DesktopMenuBarComponent = null;
+    menuItems: DesktopMenuBarItem[] = null;
+
+    constructor(
+      private _applicationRef: ApplicationRef,
+      private _resolver: ComponentFactoryResolver,
+      private _injector: Injector
+    ) {}
+
+    setMenuBarItems(items: DesktopMenuBarItem[]) {
+        this.menuItems = items;
+        this.updateMenuBarItemsInComponent();
+    }
+
+    updateMenuBarItemsInComponent() {
+        if (this.menuComponentInstance) {
+            this.menuComponentInstance.items = this.menuItems;
+        }
+    }
+
+    showMenuBar() {
+      const componentFactory = this._resolver.resolveComponentFactory(DesktopMenuBarComponent);
+      const componentRef = componentFactory.create(this._injector);
+      this._applicationRef.attachView(componentRef.hostView);
+      this.moveToDocumentBody(componentRef);
+
+      this.menuComponentInstance = componentRef.instance;
+      this.updateMenuBarItemsInComponent();
+    }
+
+    public moveToElement<T>(componentRef: ComponentRef<T>, element: Element): void {
+      element.insertBefore(componentRef.location.nativeElement, element.firstChild);
+    }
+
+    // Moves the component to the document body.
+    public moveToDocumentBody<T>(componentRef: ComponentRef<T>): void {
+      this.moveToElement(componentRef, document.querySelector('body')!);
+  }
 }
--- projects/desktop-menu-bar/src/public-api.ts
+++ projects/desktop-menu-bar/src/public-api.ts
@@ -1,3 +1,4 @@
 export * from './lib/desktop-menu-bar/desktop-menu-bar.component';
 export * from './lib/desktop-menu-bar.module';
+export * from './lib/desktop-menu-bar.service';
 export * from './interfaces/desktop-menu-bar-item.interface';
